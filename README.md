80fd74835a1d0c20799d0695530f6142b7cd2704
farrasfadhils <farrasfadhil06@gamil.com>
2026-04-13 07:46:17 +0700

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# proyek_4_poki_polban_kita
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/proyek_4_poki_polban_kita/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proyek_4_poki_polban_kita
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
